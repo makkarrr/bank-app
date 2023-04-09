cbb770dc8fdd2029eeac06bdf5601c23d1f0acbf
Kanav  Puri <kanavslaptop@Kanavs-Laptop.local>
2023-04-09 21:03:00 +0530

    First Commit - added comments

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        // we'll be creating a bank app today
+        // features to be added -
+        // 0. create bank account
+        // 1. withdraw balance
+        // 2. add balance
+        // 3. check balance
+        System.out.println("first project - bank app");
     }
 }
